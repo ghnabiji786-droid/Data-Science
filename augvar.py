458f0fe5b849a4df96438f1bbac901a10d0fb752
unknown <ghnabiji786@gmail.com>
2026-08-02 19:59:31 +0530

    Add augvar.py

---
+++ augvar.py
@@ -0,0 +1,143 @@
+#TYPE CASTING
+
+#string to integer
+# age="25"
+# age=int(age)
+# print(age)
+# print(type(age))
+
+# integer to float
+# num = 10
+# num = float(num)
+# print(num)
+# print(type(num))
+
+# float to int
+# price = 99.99
+# price = int(price)
+# print(price)
+
+
+#INDEX OF A STR
+
+# name = "python"
+# print (name[0])
+# print (name[3])
+# print (name[5])
+
+            
+#RANGE
+
+# for i in range(5):
+#  print(i)
+# for i in range(2,6):
+#  print(i)
+# for i in range(2,11,2):
+#   print(i)
+
+#SPLIT
+# text = "Sky is full of Amazing creation"
+# words = text.split()
+# print(words)
+
+# text = "Adjuvants,Haptens,Paratopes"
+# terms = text.split(",")
+# print(terms)
+
+# date ="31-07-2026"
+# parts = date.split("-")
+# print (parts)
+
+# data = "Rana,25,Morocco"
+# info = data.split(",")
+# print(info[0])
+# print(info[1])
+# print(info[2])
+
+#ARITHMETIC OPERATIONS
+
+# #EXPONENTS
+# a=10
+# print(a*a*a)
+# print(a**8)
+
+#FLOOR DIVISION
+# a=6
+# b=7
+# print(a//b)
+
+#RELATIONAL OPERATORS
+#is it equal?
+# a=10
+# print(a==10)
+# a=15
+# print(a==18)
+ 
+#is it unequal
+# a=14
+# print(a!=13)
+# a=56
+# print(a!=56)
+
+# a=56
+# print(a>7)
+
+# a=56
+# print(a>=56)
+
+# a=98
+# print(a<100)
+
+# a=78
+# print(a<=78)
+
+#ASSIGNMENT OPERATORS.....assign value to an operator
+
+# a=10
+# a=a+5
+# print(a)
+# a=78
+# a+=10
+# print(a)
+
+# a=17
+# a-=7
+# print(a)
+
+# a=78
+# a*=2
+# print(a)
+
+# a=76
+# a/=2
+# print(a)
+
+# a=98
+# a//=3
+# print(a)
+
+#DATA TYPES....nature of data variable withholds
+# int....integer
+# float..whole number
+# str....textual data
+# bool...true or false
+
+# a="if i were Macbeth,would i listen to my lady?"
+# print(type(a))
+
+# a=65
+# print(type(a))
+
+# a=9.2
+# print(type(a))
+
+#why is this not showing bool?
+# a=67
+# a>=67
+# print(type(a))
+
+#dtp 
+#"" equals to ''
+
+
+
